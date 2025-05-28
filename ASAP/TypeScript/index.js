7f427031a775b7940d694b7d83021b85ca3c584f
Alexdb2705 <alejandro.barrioa@uah.es>
2025-05-28 10:31:53 +0200

    Basic types and tsc usage

---
+++ ASAP/TypeScript/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Basic Types
+let age = 29;
+let course = "TypeScript";
+let lessonCompleted = true;
+let x = "Not recommended because TypeScript power is when types are set";
+let courses = ["HTML", "CSS", "JS", "TypeScript"];
+let arr = [true, "123", 321];
+let stringArr = [['a'], ['b']];
+let arrofarr = [[true], ['c'], [123]];
+// Tuples
---
+++ ASAP/TypeScript/index.ts
@@ -0,0 +1,12 @@
+// Basic Types
+let age: number = 29
+let course: string = "TypeScript"
+let lessonCompleted: boolean = true
+let x: any = "Not recommended because TypeScript power is when types are set"
+
+let courses: string[] = ["HTML", "CSS", "JS", "TypeScript"]
+let arr: any[] = [true, "123", 321]
+let stringArr: string[][] = [['a'], ['b']]
+let arrofarr: any[][] = [[true], ['c'], [123]]
+
+// Tuples
